2e0af39fc19d261e5cd0d1f3e39ba0b5322f6fb3
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-25 15:53:04 +0200

    exo2

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice 2</title>
+</head>
+<body>
+  <p>
+    <?php
+    $lastname = 'Cousin Ruby';
+    $firstname = 'Cédric';
+    $age = 34;
+
+    echo $lastname;
+    echo $firstname;
+    echo $age;
+
+    ?>
+  </p>
+</body>
+</html>
